ccb66705c89f2cc81c8ae1889a07c63c25a6c9b7
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-10-26 00:56:09 +0200

    Updated JS scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
 // TODAY'S DATE
 
-var date = new Date().toLocaleDateString('en-GB', {
-  weekday: 'long', 
-  year: 'numeric', 
-  month: 'long', 
-  day: 'numeric'
+let date = new Date().toLocaleDateString('en-GB', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
 });
 
 document.getElementById('date').innerHTML = date;
-
 
 // CATEGORIES
 
@@ -19,21 +18,20 @@ const getCategories = async () => {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
     },
   })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Categories:', data.length);
-    document.getElementById('categories').innerHTML = data.length;
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Categories:', data.length);
+      document.getElementById('categories').innerHTML = data.length;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 };
 
 getCategories();
-
 
 // EXCHANGES
 
@@ -44,21 +42,20 @@ const getExchanges = async () => {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
     },
   })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Exchanges:', data.length);
-    document.getElementById('exchanges').innerHTML = data.length;
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Exchanges:', data.length);
+      document.getElementById('exchanges').innerHTML = data.length;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 };
 
 getExchanges();
-
 
 // ASSET PLATFORMS
 
@@ -69,21 +66,20 @@ const getPlatforms = async () => {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
     },
   })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Platforms:', data.length);
-    document.getElementById('platforms').innerHTML = data.length;
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Platforms:', data.length);
+      document.getElementById('platforms').innerHTML = data.length;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 };
 
 getPlatforms();
-
 
 // INDEXES
 
@@ -94,42 +90,56 @@ const getIndexes = async () => {
     headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
     },
   })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Indexes:', data.length);
-    document.getElementById('indexes').innerHTML = data.length;
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Indexes:', data.length);
+      document.getElementById('indexes').innerHTML = data.length;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 };
 
 getIndexes();
 
-
 // CRYPTOCURRENCY TABLE
 
 const cryptoDiv = document.getElementById('crypto');
-let tableHeaders = ['ID', 'Image', 'Name', 'Symbol', 'Price', '24h', 'Volume', 'Market Cap'];
+let tableHeaders = [
+  'ID',
+  'Image',
+  'Name',
+  'Symbol',
+  'Price',
+  '24h',
+  'Volume',
+  'Market Cap',
+];
 
 const createCryptoTable = () => {
   while (cryptoDiv.firstChild) {
     cryptoDiv.removeChild(cryptoDiv.firstChild);
   }
-  
+
   let cryptoTable = document.createElement('table');
-  cryptoTable.classList.add('table', 'table-dark', 'table-hover', 'border-light', 'cryptoTable');
-  
+  cryptoTable.classList.add(
+    'table',
+    'table-dark',
+    'table-hover',
+    'border-light',
+    'cryptoTable'
+  );
+
   let cryptoTableHead = document.createElement('thead');
   cryptoTableHead.className = 'cryptoTableHead';
-  
+
   let cryptoTableHeaderRow = document.createElement('tr');
   cryptoTableHeaderRow.className = 'cryptoTableHeaderRow';
 
-  tableHeaders.forEach(header => {
+  tableHeaders.forEach((header) => {
     let cryptoHeader = document.createElement('th');
     cryptoHeader.innerHTML = header;
     cryptoTableHeaderRow.append(cryptoHeader);
@@ -142,7 +152,7 @@ const createCryptoTable = () => {
   cryptoTableBody.className = 'cryptoTableBody';
   cryptoTable.append(cryptoTableBody);
   cryptoDiv.append(cryptoTable);
-}
+};
 
 const appendCrypto = (singleCoin, singleCoinID) => {
   const cryptoTable = document.querySelector('.cryptoTableBody');
@@ -151,7 +161,7 @@ const appendCrypto = (singleCoin, singleCoinID) => {
 
   let coinID = document.createElement('td');
   coinID.innerHTML = singleCoinID;
-  
+
   let coinImage = document.createElement('td');
   let image = document.createElement('img');
   image.src = singleCoin.image;
@@ -170,50 +180,54 @@ const appendCrypto = (singleCoin, singleCoinID) => {
 
   let coin24h = document.createElement('td');
   coin24h.innerHTML = singleCoin.price_change_percentage_24h.toFixed(2) + '%';
-  coin24h.style.color = singleCoin.price_change_percentage_24h > 0 ? '#2e7d32' : '#d50000';
+  coin24h.style.color =
+    singleCoin.price_change_percentage_24h > 0 ? '#2e7d32' : '#d50000';
 
   let coinVolume = document.createElement('td');
   coinVolume.innerHTML = '$' + singleCoin.total_volume.toLocaleString();
 
   let coinMarketCap = document.createElement('td');
-  coinMarketCap.innerHTML = '$' + singleCoin.market_cap.toLocaleString()
+  coinMarketCap.innerHTML = '$' + singleCoin.market_cap.toLocaleString();
 
   cryptoTableBodyRow.append(
     coinID,
     coinImage,
     coinName,
     coinSymbol,
-    coinCurrentPrice, 
+    coinCurrentPrice,
     coin24h,
     coinVolume,
-    coinMarketCap,
+    coinMarketCap
   );
 
   cryptoTable.append(cryptoTableBodyRow);
 };
 
 const getCrypto = async () => {
-  await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=5&page=1&sparkline=false', {
-    method: 'GET',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods':'GET, POST, PUT, DELETE',
-    },
-  })
-  .then((response) => response.json())
-  .then((coins) => {
-    console.log('Crypto Data:', coins);
-    createCryptoTable();
-    for (const coin of coins) {
-      let coinID = coins.indexOf(coin) + 1;
-      appendCrypto(coin, coinID);
+  await fetch(
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=5&page=1&sparkline=false',
+    {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+      },
     }
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+  )
+    .then((response) => response.json())
+    .then((coins) => {
+      console.log('Crypto Data:', coins);
+      createCryptoTable();
+      for (const coin of coins) {
+        let coinID = coins.indexOf(coin) + 1;
+        appendCrypto(coin, coinID);
+      }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 };
 
 getCrypto();
